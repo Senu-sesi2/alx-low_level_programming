b02a14a6a223ec0fd292d7b66968317a19550136
Senu-e30 <senu.emmanuel1993@gmail.com>
2023-09-10 14:15:01 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet
+ * followed by uppercase alphabets
+ * Return: program returns 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
