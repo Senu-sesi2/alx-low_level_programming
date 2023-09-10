a6a0bd2d2f2dbc9eae465889420e0e68e1128d83
Senu-e30 <senu.emmanuel1993@gmail.com>
2023-09-10 16:45:51 +0000

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - print all possible different combinations of three digits.
+ * Return: program returns 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		int i = n;
+		while (i <= 9)
+		{
+			int j = i;
+			while (j <= 9)
+			{
+				if (n != i && i != j)
+				{
+					putchar (n % 10 + '0');
+					putchar (i % 10 + '0');
+					putchar (j % 10 + '0');
+					if (n != 7)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+				j++;
+			}
+			i++;
+		}
+		n++;
+	}
+	putchar ('\n');
+	return (0);
+}
