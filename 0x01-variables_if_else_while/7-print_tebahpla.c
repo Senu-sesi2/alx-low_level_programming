eccfbd70d17fa833c5a87e65a6fb3e3c8acdfd55
Senu-e30 <senu.emmanuel1993@gmail.com>
2023-09-10 15:28:34 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet in reverse
+ * Return: program returns 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
