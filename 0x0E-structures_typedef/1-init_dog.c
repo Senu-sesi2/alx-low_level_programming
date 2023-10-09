e783004e9d804585a0e104647adae50df793a226
Senu-e30 <senu.emmanuel1993@gmail.com>
2023-10-09 22:54:09 +0000

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,13 @@
 #include "dog.h"
 
 /**
- * init_dog i initialize dog structure
- *
- * @d: structure object
- * @name: 1st member
- * @age: 2nd member
- * @owner: 3rd member
- */
+* init_dog - initialize dog structure
+*
+* @d: structure object
+* @name: 1st member
+* @age: 2nd member
+* @owner: 3rd member
+*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
