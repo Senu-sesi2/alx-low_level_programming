6cee428a4b7718e3c5d3e6d41e6d5d312d8f53cf
Senu-e30 <senu.emmanuel1993@gmail.com>
2023-10-30 22:06:54 +0000

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+/*
+ * File: main.h
+ * Auth: Senu
+ * Desc: Header file containing prototypes for all functions
+ * written in the 0x15-file_io directory.
+ */
 
-#include <fct1.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
